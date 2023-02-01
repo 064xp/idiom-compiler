c75cbcc0a5e269145dd9c6c82f15aadd3672d74d
Paulo <paulozamora2000@gmail.com>
2023-02-01 16:47:42 -0600

    File input button and read from file

--- src/ components/CircularButton/CircularButton.tsx
+++ src/ components/CircularButton/CircularButton.tsx
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./CircularButton.module.css";
 import clsx from "clsx";
 
-interface IProps {
+type Props = React.HTMLAttributes<HTMLButtonElement> & {
   icon: string;
   size?: number | string;
   color?: string;
   alt?: string;
   className?: string;
-}
+  isFileInput?: boolean;
+  fileInputAttrs?: React.HTMLAttributes<HTMLInputElement>;
+};
 
 const CircularButton = ({
   size = "4em",
@@ -16,15 +18,38 @@ const CircularButton = ({
   color,
   alt,
   className,
+  isFileInput,
+  fileInputAttrs,
   ...rest
-}: IProps) => {
+}: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const onClick: React.MouseEventHandler<HTMLButtonElement> = (ev) => {
+    if (isFileInput) ref.current?.click();
+
+    if (rest.onClick) rest.onClick(ev);
+  };
+
   return (
     <button
+      style={{
+        height: size,
+        width: size,
+        backgroundColor: color || "",
+      }}
       className={clsx(styles.button, className)}
-      style={{ height: size, width: size, backgroundColor: color || "" }}
+      onClick={onClick}
       {...rest}
     >
-      <img src={icon} alt={alt || "Button"} />
+      <img src={icon} alt={alt} />
+      {isFileInput && (
+        <input
+          type="file"
+          ref={ref}
+          className={styles.fileInput}
+          {...fileInputAttrs}
+        />
+      )}
     </button>
   );
 };
--- src/ components/MainToolbar/MainToolbar.tsx
+++ src/ components/MainToolbar/MainToolbar.tsx
@@ -6,12 +6,45 @@ import RunIcon from "../../assets/play-solid.svg";
 import LoadIcon from "../../assets/file-solid.svg";
 import SaveIcon from "../../assets/floppy-disk-solid.svg";
 
+interface HTMLInputEvent extends React.FormEvent<HTMLInputElement> {
+  target: HTMLInputElement & EventTarget;
+}
+
 const MainToolbar = () => {
+  const onFileInputChanged: React.FormEventHandler<HTMLInputElement> = (
+    event: HTMLInputEvent
+  ) => {
+    const fileList = event.target.files;
+    if (!fileList || fileList?.length == 0) return;
+
+    const file = fileList[0];
+
+    // read file
+    const reader = new FileReader();
+    reader.addEventListener("load", (event) => {
+      if (!event.target) return;
+      console.log(event.target.result);
+    });
+
+    reader.readAsText(file);
+  };
+
   return (
     <div className={styles.container}>
-      <CircularButton icon={RunIcon} size="5.2em" />
-      <CircularButton icon={LoadIcon} />
-      <CircularButton icon={SaveIcon} />
+      <CircularButton
+        icon={RunIcon}
+        size="5.2em"
+        className={styles.mainButtons}
+      />
+      <CircularButton
+        icon={LoadIcon}
+        className={styles.mainButtons}
+        isFileInput={true}
+        fileInputAttrs={{
+          onChange: onFileInputChanged,
+        }}
+      />
+      <CircularButton icon={SaveIcon} className={styles.mainButtons} />
     </div>
   );
 };
